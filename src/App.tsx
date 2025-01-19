f463ab94386b0539a26c13cd8a47340287cab302
Ryan <ryanshaunsmith@gmail.com>
2025-01-19 12:56:17 +0000

    update responsive breakpoints for slick slider

--- src/App.tsx
+++ src/App.tsx
@@ -31,22 +31,14 @@ function App() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
           infinite: true,
           dots: true
         }
       },
       {
         breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
