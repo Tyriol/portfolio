9ea9874af0f81fd101f5ad6848e70463a931d44d
Ryan <ryanshaunsmith@gmail.com>
2025-03-24 19:53:18 +0000

    add two new projects

--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -1,46 +1,69 @@
-import { ProjectType } from "../types/types"
+import { ProjectType } from "../types/types";
 
 const projects: ProjectType[] = [
-    {
-        title: "METIS: Study Buddy",
-        description: "Our School of Code final project. From conception to delivery in 4 days leveraging new found knowledge, collaboration and grit (And Next, JS and Supabase)",
-        image:"../metis.png",
-        imageAltText: "Metis Logo",
-        githubLink: "https://github.com/Tyriol/metis-study-buddy",
-        websiteLink: "https://metis.saffagonerogue.me"
-    },
-    {
-        title: "The Learning Audit",
-        description: "A side project I've been using to apply new concepts to as I learn them, with the goal of having an app that can track your learning journey.",
-        image: "../learning-audit.png",
-        imageAltText: "The Learning Audit homepage",
-        githubLink: "https://github.com/Tyriol/learning-audit",
-        websiteLink: "https://learning-audit.saffagonerogue.me"
-    },
-    {
-        title: "Quiz Academy", 
-        description: "Built early on in using vanilla JavaScript. This gave me my first proper taste of how interesting and challenging coding can be. A useful playground for trying new things.", 
-        image: "../quiz.png", 
-        imageAltText: "The Quiz Academy homepage", 
-        githubLink: "https://github.com/Tyriol/quizAcademy", 
-        websiteLink: "https://quiz.saffagonerogue.me"
-    },
-    {
-        title: "TaskLite", 
-        description: "Obligatory todo app, built using TypeScript and the Vite React framework. Made to limit todo's in progress and I learnt lots about using TS with React", 
-        image: "../tasklite.png", 
-        imageAltText: "The TaskLite App", 
-        githubLink: "https://github.com/Tyriol/taskLite", 
-        websiteLink: "https://todo.saffagonerogue.me/"
-    },
-    {
-        title: "Blackjack", 
-        description: "A tech test, built using a combination of vanilla JavaScript and React using Vite. I learnt a lot about testing React components using Vitest", 
-        image: "../blackjack.png", 
-        imageAltText: "The TaskLite App", 
-        githubLink: "https://github.com/Tyriol/blackjack-bbc-segs-tech-test", 
-        websiteLink: "https://blackjack.saffagonerogue.me/"
-    }
-]
+  {
+    title: "METIS: Study Buddy",
+    description:
+      "Our School of Code final project. From conception to delivery in 4 days leveraging new found knowledge, collaboration and grit (And Next, JS and Supabase)",
+    image: "../metis.png",
+    imageAltText: "Metis Logo",
+    githubLink: "https://github.com/Tyriol/metis-study-buddy",
+    websiteLink: "https://metis.saffagonerogue.me",
+  },
+  {
+    title: "The Learning Audit",
+    description:
+      "A side project I've been using to apply new concepts to as I learn them, with the goal of having an app that can track your learning journey.",
+    image: "../learning-audit.png",
+    imageAltText: "The Learning Audit homepage",
+    githubLink: "https://github.com/Tyriol/learning-audit",
+    websiteLink: "https://learning-audit.saffagonerogue.me",
+  },
+  {
+    title: "Quiz Academy",
+    description:
+      "Built early on in using vanilla JavaScript. This gave me my first proper taste of how interesting and challenging coding can be. A useful playground for trying new things.",
+    image: "../quiz.png",
+    imageAltText: "The Quiz Academy homepage",
+    githubLink: "https://github.com/Tyriol/quizAcademy",
+    websiteLink: "https://quiz.saffagonerogue.me",
+  },
+  {
+    title: "TaskLite",
+    description:
+      "Obligatory todo app, built using TypeScript and the Vite React framework. Made to limit todo's in progress and I learnt lots about using TS with React",
+    image: "../tasklite.png",
+    imageAltText: "The TaskLite App",
+    githubLink: "https://github.com/Tyriol/taskLite",
+    websiteLink: "https://todo.saffagonerogue.me/",
+  },
+  {
+    title: "Blackjack",
+    description:
+      "A tech test, built using a combination of vanilla JavaScript and React using Vite. I learnt a lot about testing React components using Vitest",
+    image: "../blackjack.png",
+    imageAltText: "The Blackjack game",
+    githubLink: "https://github.com/Tyriol/blackjack-bbc-segs-tech-test",
+    websiteLink: "https://blackjack.saffagonerogue.me/",
+  },
+  {
+    title: "Tenzies",
+    description:
+      "A reasonably simple game I built off the back of the Scrimba 'Learn React' course, rewritten outside of the course using TypeScript with added roll count and timer",
+    image: "../tenzies.png",
+    imageAltText: "The Tenzies Game",
+    githubLink: "https://github.com/Tyriol/tenzies",
+    websiteLink: "https://peppy-croissant-378d4e.netlify.app/",
+  },
+  {
+    title: "Assembly: Endgame",
+    description:
+      "Another game I built off the back of the Scrimba 'Learn React' course, rewritten outside of the course using TypeScript",
+    image: "../assembly.png",
+    imageAltText: "The Assembly: Endgame App",
+    githubLink: "https://github.com/Tyriol/assembly-hangman",
+    websiteLink: "https://super-sprite-84c4ed.netlify.app/",
+  },
+];
 
 export default projects;
